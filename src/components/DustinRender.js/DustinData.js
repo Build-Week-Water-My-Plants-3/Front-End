a57b04f7db1c8fa939ba7b80e9d3609807252ab4
unknown <Dustin.Guillen14@yahoo.com>
2020-03-10 12:58:09 -0600

    MVP

--- src/components/DustinRender.js/DustinData.js
+++ src/components/DustinRender.js/DustinData.js
@@ -1,24 +1,29 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
 
-import stuff from "../../mockData/mockPlants";
+//import stuff from "../../mockData/mockPlants";
 
 function Data(){
     const [Data, setData] = useState([]);
 
     useEffect(() => {
         axios
-        .post("https://water-my-plants-bw-3.herokuapp.com/plants/all")
-        .then(response => setData(response))
-    })
+          .get("https://water-my-plants-bw-3.herokuapp.com/plants/all")
+          .then(response => {
+            console.log(response);
+            setData(response.data);
+          })
+          .catch(error => console.log(error));
+      }, []);
+    
 
-console.log("The DATA", stuff)
+console.log("The DATA", Data)
     return(
         <div>
            <ul>
-                {stuff.map(plant => (
+                {Data.map(plant => (
                     <li>
-                        {plant.name}
+                        {plant.nickname}
                     </li>
                     
                 ))}
